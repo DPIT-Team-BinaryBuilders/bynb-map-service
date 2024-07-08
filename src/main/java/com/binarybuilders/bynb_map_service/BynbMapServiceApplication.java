c351349e2a7900338b052c2732f8c5b13cfa83db
Berger Radu <raduberger9@gmail.com>
2024-07-08 12:43:00 +0300

    Initial test commit

---
+++ src/main/java/com/binarybuilders/bynb_map_service/BynbMapServiceApplication.java
@@ -0,0 +1,13 @@
+package com.binarybuilders.bynb_map_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BynbMapServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BynbMapServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/binarybuilders/bynb_map_service/BynbMapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binarybuilders.bynb_map_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BynbMapServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
